7fafd469936f57217a486a4fa2f1182d88f17aff
Asher120 <ukingv1209@gmail.com>
2023-10-07 15:48:09 +0200

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+i#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Uses function puts on print expected output to screen
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Stdout is an integer
+ * Description: Takes stdinput and prints a string on display
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("With proper grammar,  but the output is a piece of art,\n");
+	return (0);
+}
